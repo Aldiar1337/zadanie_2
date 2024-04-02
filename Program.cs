439cef3a8667141cce1749f142448322e60f2b26
aldiar1337 <vitugov2005@gmail.com>
2024-04-02 19:56:48 +0700

    poyavis

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp5;
+
+class Program
+{
+    static void Main()
+    {
+        Student student = Student.ReadStudent();
+        student.PrintInfo();
+        Console.Write("Введите новую фамилию: ");
+        student.surname = Console.ReadLine();
+        Console.Write("Введите новую дату рождения (в формате дд.мм.гггг): ");
+        student.birthdate = DateTime.ParseExact(Console.ReadLine(), "dd.MM.yyyy", null);
+        Console.Write("Введите новый номер группы: ");
+        student.group = Console.ReadLine();
+        student.PrintInfo();
+    }
+}
