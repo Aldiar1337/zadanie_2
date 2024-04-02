6a34048ceec527213d744cf7e98b6f9a79e697a4
aldiar1337 <vitugov2005@gmail.com>
2024-04-02 22:03:14 +0700

    provekra

--- Program2.cs
+++ Program2.cs
@@ -1,16 +1,2 @@
 ﻿namespace ConsoleApp5;
 
-class Program2
-{
-     public class Train
-    {
-        public string Destination;
-        public int Number;
-        public string DepartureTime;
-
-        public void DisplayTrainInfo()
-        {
-            Console.WriteLine($"Поезд с номером {Number} следует в {Destination} и отправляетсяя в {DepartureTime}");
-        }
-    }
-}
