ea6f2e83578171df5b30bc0d9ebb161a372f0ed3
aldiar1337 <vitugov2005@gmail.com>
2024-04-02 23:18:54 +0700

    4Functia

--- Program4.cs
+++ Program4.cs
@@ -2,40 +2,5 @@
 
 public class Program4
 {
-    class TwoIntegers
-    {
-        public int firstInteger;
-        public int secondInteger;
-        
-        
-        public void PrintIntegers()
-        {
-            Console.WriteLine("Первое число: " + firstInteger);
-            Console.WriteLine("Второе число: " + secondInteger);
-        }
-        
-        public void ChangeIntegers(int newFirst, int newSecond)
-        {
-        
-            firstInteger = newFirst;
-            secondInteger = newSecond;
-        }
-        
-        public int SumIntegers()
-        {
-            return firstInteger + secondInteger;
-        }
-        
-        public int MaxInteger()
-        {
-            if (firstInteger < secondInteger)
-            {
-                return (secondInteger);
-            }
-            else
-            {
-                return (firstInteger);
-            }
-        }
-    }
+    
 }
