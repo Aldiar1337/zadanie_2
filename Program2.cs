1695d543cb1916a8ed63156e382f590bd8293c7d
aldiar1337 <vitugov2005@gmail.com>
2024-04-02 20:29:18 +0700

    Functia2

--- Program2.cs
+++ Program2.cs
@@ -2,32 +2,15 @@
 
 class Program2
 {
-    static void Main()
+     public class Train
     {
-        // Создаем массив объектов Train
-        Train[] trains = new Train[]
+        public string Destination;
+        public int Number;
+        public string DepartureTime;
+
+        public void DisplayTrainInfo()
         {
-            new Train { Number = 101, Destination = "Москва", DepartureTime = "10:00" },
-            new Train { Number = 102, Destination = "Томск", DepartureTime = "14:00" },
-            new Train { Number = 103, Destination = "Новосибирск", DepartureTime = "18:00" },
-            new Train { Number = 104, Destination = "Харьков", DepartureTime = "22:00" }
-        };
-        Console.Write("Введите номер поезда для вывода информации о нем: ");
-        int trainNumber = int.Parse(Console.ReadLine());
-        bool trainFound = false;
-        foreach (Train train in trains)
-        {
-            if (train.Number == trainNumber)
-            {
-                train.DisplayTrainInfo();
-                trainFound = true;
-                break;
-            }
-        }
-   
-        if (!trainFound)
-        {
-            Console.WriteLine($" Поезд с таким номером {trainNumber} не был найден");
+            Console.WriteLine($"Поезд с номером {Number} следует в {Destination} и отправляется в {DepartureTime}");
         }
     }
 }
