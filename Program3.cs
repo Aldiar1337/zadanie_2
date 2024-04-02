1e2de1b2000c9fd90f49888e9c02cf84fc194397
aldiar1337 <vitugov2005@gmail.com>
2024-04-02 22:32:27 +0700

    Functia3

---
+++ Program3.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp5;
+
+class NamesSurname
+{
+    public String name;
+    public String surname;
+    public NamesSurname()
+    {
+        this.name = "";
+        this.surname = "";
+    }
+    public NamesSurname (String name1, String name2)
+    {
+        this.name = name1;
+        this.surname = name2;
+    }
+    ~NamesSurname()
+    {
+        Console.Write($"{this.name} was deleted");
+        Console.Write($"{this.surname} was deleted");
+    }
+}
