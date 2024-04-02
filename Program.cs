109618dfea3a91622c5059089081190abfcd5af7
aldiar1337 <vitugov2005@gmail.com>
2024-04-02 20:05:59 +0700

    cho

--- Program.cs
+++ Program.cs
@@ -2,19 +2,6 @@
 
 class Program
 {
-    static void Main()
-    {
-        Student student = Student.ReadStudent();
-        student.PrintInfo();
-        Console.Write("Введите новую фамилию: ");
-        student.surname = Console.ReadLine();
-        Console.Write("Введите новую дату рождения (в формате дд.мм.гггг): ");
-        student.birthdate = DateTime.ParseExact(Console.ReadLine(), "dd.MM.yyyy", null);
-        Console.Write("Введите новый номер группы: ");
-        student.group = Console.ReadLine();
-        student.PrintInfo();
-    }
-    
     class Student {
         public string surname;
         public DateTime birthdate;
